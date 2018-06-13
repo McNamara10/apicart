99c7d1f2197f67e47cbce16369c5f7d5a66d7db9
Marcello Orrù <marcelorru@gmail.com>
2018-06-13 12:13:35 +0200

    Initial commit

---
+++ models/Prodotti.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "prodotti".
+ *
+ * @property int $id
+ * @property string $nome
+ * @property string $descrizione
+ */
+class Prodotti extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'prodotti';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nome', 'descrizione'], 'required'],
+            [['nome', 'descrizione'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nome' => 'Nome',
+            'descrizione' => 'Descrizione',
+        ];
+    }
+}
